818520c789a18301e66be98fcfd1b82f8ecd2592
Nishant Y <nishantry3108@gmail.com>
2025-05-01 19:03:02 -0400

    Delete Menu directory

--- Menu/character_body_2d_2.gd
+++
@@ -1,7 +0,0 @@
-extends CharacterBody2D
-
-
-func _ready() -> void:
-	get_node("Zombie 1").play("Idle")
-	get_node("Zombie 2").play("Idle")
-	get_node("Zombie 3").play("Idle")
--- Menu/character_body_2d_4.gd
+++
@@ -1,6 +0,0 @@
-extends CharacterBody2D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	get_node("AnimatedSprite2D").play("Idle")
